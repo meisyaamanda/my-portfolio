58df273a42313d3d3c7a82cc934347a15db344a1
meisyaamanda <meisyaaldiamanda@gmail.com>
2025-09-21 22:41:05 +0700

    feat: add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,39 @@
+import {
+  DiscordLogoIcon,
+  EnvelopeClosedIcon,
+  GitHubLogoIcon,
+  LinkedInLogoIcon,
+} from "@radix-ui/react-icons";
+import ParticlesBg from "../home/particle-bg";
+import { FaWhatsapp } from "react-icons/fa";
+
 function ContactPage() {
-  return <div className="text-3xl text-center items-center">Contact</div>;
+  return (
+    <div className="relative">
+      <ParticlesBg />
+      <h2 className="text-center text-xl sm:text-2xl md:text-3xl font-bold text-white">
+        I’m always open to new opportunities, collaborations, or just a friendly
+        chat. Feel free to reach out through the links below:
+      </h2>
+      <div className="mt-8 pl-8 gap-8 item-center flex flex-col md:flex-row justify-center">
+        <a href="https://wa.me/6281270214415" target="_blank">
+          <FaWhatsapp className="w-12 h-12" />
+        </a>
+        <a href="https://www.linkedin.com/in/meisyaamandaaldi/" target="_blank">
+          <LinkedInLogoIcon className="w-12 h-12" />
+        </a>
+        <a href="https://github.com/meisyaamanda" target="_blank">
+          <GitHubLogoIcon className="w-12 h-12" />
+        </a>
+        <a href="https://discord.com/users/maaannn_" target="_blank">
+          <DiscordLogoIcon className="w-12 h-12" />
+        </a>
+        <a href="meiyaamandaa02@gmail.com" target="_blank">
+          <EnvelopeClosedIcon className="w-12 h-12" />
+        </a>
+      </div>
+    </div>
+  );
 }
+
 export default ContactPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Photo from "./home/display-photo/page";
 import ProjectsPage from "./project/page";
+import ContactPage from "./contact/page";
 function Page() {
   return (
     <div className="overflow-hidden">
       <Photo />
       <ProjectsPage />
+      <ContactPage />
     </div>
   );
 }
