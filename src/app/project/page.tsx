bf50bb865d5e9993f3e97891b2c032dfd440e31e
meisyaamanda <meisyaaldiamanda@gmail.com>
2025-09-21 22:53:50 +0700

    fix: change className

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -3,22 +3,27 @@ import ParticlesBg from "../home/particle-bg";
 
 function ProjectsPage() {
   return (
-    <div className="pb-16 pt-16">
+    <div className="relative min-h-screen text-white overflow-hidden py-16">
       <ParticlesBg />
-      <h1 className="text-center text-2xl sm:text-4xl md:text-5xl font-bold text-white">
+
+      {/* Title */}
+      <h1 className="text-center text-2xl sm:text-4xl md:text-5xl font-bold">
         A small selection of recent{" "}
         <span className="text-cyan-300">projects</span>
       </h1>
-      <div>
-        <div className="mt-8 pl-8 gap-8 item-center flex flex-col md:flex-row">
+
+      {/* Projects Wrapper */}
+      <div className="mt-12 space-y-12 px-6 md:px-12 lg:px-20">
+        {/* Project 1 */}
+        <div className="flex flex-col md:flex-row items-center gap-8">
           <Image
             src="/images/bei.png"
             alt="bei"
             width={800}
             height={650}
-            className="rounded-lg shadow-lg"
+            className="rounded-lg shadow-lg w-full md:w-1/2"
           />
-          <div className="text-center md:text-left">
+          <div className="text-center md:text-left md:w-1/2">
             <h2 className="text-2xl font-semibold text-white">
               PT Bursa Efek Indonesia
             </h2>
@@ -27,15 +32,17 @@ function ProjectsPage() {
             </p>
           </div>
         </div>
-        <div className="mt-8 pl-8 gap-8 item-center flex flex-col md:flex-row-reverse">
+
+        {/* Project 2 */}
+        <div className="flex flex-col md:flex-row-reverse items-center gap-8">
           <Image
             src="/images/muf.png"
             alt="muf"
             width={800}
             height={650}
-            className="rounded-lg shadow-lg"
+            className="rounded-lg shadow-lg w-full md:w-1/2"
           />
-          <div className="text-center md:text-left">
+          <div className="text-center md:text-left md:w-1/2">
             <h2 className="text-2xl font-semibold text-white">
               Mandiri Utama Finance
             </h2>
@@ -44,15 +51,17 @@ function ProjectsPage() {
             </p>
           </div>
         </div>
-        <div className="mt-8 pl-8 gap-8 item-center flex flex-col md:flex-row">
+
+        {/* Project 3 */}
+        <div className="flex flex-col md:flex-row items-center gap-8">
           <Image
             src="/images/p3.jpg"
             alt="img"
             width={800}
             height={650}
-            className="rounded-lg shadow-lg"
+            className="rounded-lg shadow-lg w-full md:w-1/2"
           />
-          <div className="text-center md:text-left">
+          <div className="text-center md:text-left md:w-1/2">
             <h2 className="text-2xl font-semibold text-white">
               PT Sarana Multi Infrastruktur
             </h2>
@@ -61,15 +70,17 @@ function ProjectsPage() {
             </p>
           </div>
         </div>
-        <div className="mt-8 pl-8 gap-8 item-center flex flex-col md:flex-row-reverse">
+
+        {/* Project 4 */}
+        <div className="flex flex-col md:flex-row-reverse items-center gap-8">
           <Image
             src="/images/muf.png"
             alt="muf"
             width={800}
             height={650}
-            className="rounded-lg shadow-lg"
+            className="rounded-lg shadow-lg w-full md:w-1/2"
           />
-          <div className="text-center md:text-left">
+          <div className="text-center md:text-left md:w-1/2">
             <h2 className="text-2xl font-semibold text-white">
               Nuxa Skin Clinic
             </h2>
@@ -78,15 +89,17 @@ function ProjectsPage() {
             </p>
           </div>
         </div>
-        <div className="mt-8 pl-8 gap-8 item-center flex flex-col md:flex-row">
+
+        {/* Project 5 */}
+        <div className="flex flex-col md:flex-row items-center gap-8">
           <Image
             src="/images/p3.jpg"
             alt="img"
             width={800}
             height={650}
-            className="rounded-lg shadow-lg"
+            className="rounded-lg shadow-lg w-full md:w-1/2"
           />
-          <div className="text-center md:text-left">
+          <div className="text-center md:text-left md:w-1/2">
             <h2 className="text-2xl font-semibold text-white">
               Lavees Cosmedics
             </h2>
